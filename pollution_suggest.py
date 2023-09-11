40951844319751f07d534fa22f931421ede39e24
Danish Adellio Alesaka <94503315+hayabusa1365@users.noreply.github.com>
2023-09-11 17:34:29 +0700

    Create pollution_suggest.py

---
+++ pollution_suggest.py
@@ -0,0 +1,3 @@
+@bot.command()
+async def pollution_suggest(ctx):
+    await ctx.send(f"Stay Informed Stay updated on air quality levels in your area. Many countries and cities have websites or mobile apps that provide real-time air quality information. Use these resources to plan your outdoor activities. Limit Outdoor Activities on Poor Air Quality Days These devices can help remove indoor air pollutants, making your indoor air cleaner and safer to breathe.")
